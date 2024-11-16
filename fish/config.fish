64ce9523b9917eb13b199597225618dff1def026
jjmurph33 <104844163+jjmurph33@users.noreply.github.com>
2024-11-16 09:45:11 -0500

    vim

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     fish_vi_key_bindings
-    abbr t tmux_attach
-    abbr f fzf --preview="batcat --color=always {}"
+    abbr -a t tmux_attach
+    abbr -a f 'fzf --preview="batcat --color=always {}"'
     set -x PATH ~/.local/bin $PATH
 end
