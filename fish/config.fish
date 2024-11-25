4abd11322d164b55239fa765ba2251148fe015dc
john <john@example.com>
2024-11-24 20:51:39 -0500

    fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,13 @@
 if status is-interactive
     fish_default_key_bindings
-    abbr -a t tmux_attach
-    abbr -a f 'fzf --preview="batcat --color=always {}"'
-    abbr -a clip 'xclip -selection clipboard'
+    set fish_greeting ''
+
     set -x PATH ~/.local/bin $PATH
     set -x PATH ~/opt/nvim/bin $PATH
+
+    abbr -a clip 'xclip -selection clipboard'
+
+    abbr -a t tmux_attach
+    abbr -a f 'fzf --preview="batcat --color=always {}"'
+    abbr -a so 'source .venv/bin/activate.fish'
 end
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,5 +1,6 @@
 function fish_prompt --description 'Write out the prompt'
     set -l last_status $status
+    set -x fish_prompt_pwd_dir_length 0 # don't truncate path
 
     prompt_login
 
