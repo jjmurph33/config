99a69ea54db04fe73139464e43775a1b10102582
john <john@example.com>
2024-11-24 19:48:33 -0500

    clip

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     fish_default_key_bindings
     abbr -a t tmux_attach
     abbr -a f 'fzf --preview="batcat --color=always {}"'
+    abbr -a clip 'xclip -selection clipboard'
     set -x PATH ~/.local/bin $PATH
     set -x PATH ~/opt/nvim/bin $PATH
 end
